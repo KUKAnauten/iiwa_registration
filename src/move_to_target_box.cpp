55c2f7a97cfd514ae53d4d9644c27b926f3faf48
Laura Bielenberg <lauramaria.bielenberg@stud.tu-darmstadt.de>
2018-01-23 15:45:27 +0100

    can now reach dummy

--- src/move_to_target_box.cpp
+++ src/move_to_target_box.cpp
@@ -36,12 +36,17 @@
 
 /*TODO*/
 /*
-- move iiwa near box (with (new) tool attached
+- Ausrichten (Quaternionen) -> als reconfig. Parameter? ->needle_chanal_pose über topic?
+- Block weg
+- Ziel in Block
+- bewegung zum ziel im Block (constraint in Z)
+--> Das in Imp.Mode?!
 
 */
 
 #include <iimoveit/robot_interface.h>
-#include <iiwa_registration/button_listener.h>
+//#include <iiwa_registration/button_listener.h>
+
 
 namespace move_to_target{
 
@@ -59,13 +64,25 @@ public:
     base_pose_.orientation.y = 1.0;
     base_pose_.orientation.z = 0.0;
     base_pose_.orientation.w = 0.0;
+
+    //Position and Orientation of the Injectionaxis (seen as a new x-axis off the Entrypoint coordinatesystem) relative to the dummy Frame
+
+    needle_chanal_.position.x = 0.01; //values need to be fixed
+    needle_chanal_.position.y = 0.01;
+    needle_chanal_.position.z = 0.0;
+    needle_chanal_.orientation.x = 0.0;
+    needle_chanal_.orientation.y = 0.0;
+    needle_chanal_.orientation.z = 0.0;
+    needle_chanal_.orientation.w = 0.0;
+
+   
   }
 
   void buttonEventCallback(const std_msgs::String::ConstPtr& msg) {
     if(msg->data == "pose_get_pressed") {
       ROS_INFO("Der Schmartie funktioniert: %s", msg->data.c_str());
-      current_pose = getPose();
-      ROS_INFO("Current Position = (%f, %f, %f)", current_pose.pose.position.x, current_pose.pose.position.y, current_pose.pose.position.z);
+      dummy_pose = getPose();
+      ROS_INFO("dummy Position = (%f, %f, %f)", dummy_pose.pose.position.x, dummy_pose.pose.position.y, dummy_pose.pose.position.z);
       visual_tools_.publishText(text_pose_, "Pose registered!", rvt::WHITE, rvt::XLARGE);
     }
   }
@@ -74,70 +91,93 @@ public:
     planAndMove(base_pose_, std::string("base pose"));
     visual_tools_.publishText(text_pose_, "I'm at my base pose...press NEXT to get to the registered pose", rvt::WHITE, rvt::XLARGE);
   }
+  
+  //Move to a point above dummy, and get the right orientation (calculations in the making)
+  void moveToNeedletarget() {
 
-  void moveToRegisteredPose() {
-    geometry_msgs::Pose registered_pose_= current_pose.pose;
-    planAndMove(registered_pose_, std::string("registered pose"));
-    visual_tools_.publishText(text_pose_, "Arrived at registered pose", rvt::WHITE, rvt::XLARGE);
+      needle_chanal_pose_.position.x = box_pose.position.x + 0.0;
+      needle_chanal_pose_.position.y = box_pose.position.y + 0.0;
+      needle_chanal_pose_.position.z = box_pose.position.z + 0.05; //needle_chanal_pose is rel. to dummy_pose
+      needle_chanal_pose_.orientation.x = box_pose.orientation.x;
+      needle_chanal_pose_.orientation.y = box_pose.orientation.y;
+      needle_chanal_pose_.orientation.z = box_pose.orientation.z;
+      needle_chanal_pose_.orientation.w = box_pose.orientation.w;
+
+     planAndMove(needle_chanal_pose_, std::string("registered pose"));
+     visual_tools_.publishText(text_pose_, "Arrived at registered pose", rvt::WHITE, rvt::XLARGE);
+   }
+
+  // Only if no boxes are used in MoveIt
+  // void moveToRegisteredPose() {
+  //   planAndMove(registered_pose_, std::string("registered pose"));
+  //   visual_tools_.publishText(text_pose_, "Arrived at registered pose", rvt::WHITE, rvt::XLARGE);
+  // }
+
+
+  
+  void spawndummy() {
+
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+    moveit_msgs::CollisionObject collision_object;
+    collision_object.header.frame_id = "/iiwa_link_0";
+    collision_object.id = "tissue_dummy";
+
+  //Silicondummy
+    shape_msgs::SolidPrimitive primitive;
+    primitive.type = primitive.BOX;
+    primitive.dimensions.resize(3);
+    primitive.dimensions[0] = 0.102;
+    primitive.dimensions[1] = 0.05;
+    primitive.dimensions[2] = 0.05;
+
+    box_pose.position.x = dummy_pose.pose.position.x;
+    box_pose.position.y = dummy_pose.pose.position.y;
+    box_pose.position.z = dummy_pose.pose.position.z;
+    box_pose.orientation.x = dummy_pose.pose.orientation.x;
+    box_pose.orientation.y = dummy_pose.pose.orientation.y;
+    box_pose.orientation.z = dummy_pose.pose.orientation.z;
+    box_pose.orientation.w = dummy_pose.pose.orientation.w;
+
+    collision_object.primitives.push_back(primitive);
+    collision_object.primitive_poses.push_back(box_pose);
+    collision_object.operation = collision_object.ADD;
+
+    std::vector<moveit_msgs::CollisionObject> collision_objects;
+    collision_objects.push_back(collision_object);
+    std::cout << "collision_objects.size(): " << collision_objects.size() << std::endl;
+    std::cout << collision_objects[0] << std::endl;
+    planning_scene_interface.applyCollisionObjects(collision_objects);
+
+    ros::Duration(2.0).sleep();
+
+    std::vector<std::string> obj = planning_scene_interface.getKnownObjectNames();
+    if (obj.size() > 0)
+    {
+      std::cout << std::endl << "-- KNOWN COLLISION OBJECTS --" << std::endl;
+      for (int i = 0; i < obj.size(); ++i)
+        std::cout << obj[i] << std::endl;
+    }
+
+
+    ROS_INFO_NAMED("tutorial", "Add an object into the world");
+    planning_scene_interface.addCollisionObjects(collision_objects);
+
+    ros::Duration(1.0).sleep();
+
   }
-
- void spawnDummy() {
-
-  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-  moveit_msgs::CollisionObject collision_object;
-  collision_object.header.frame_id = "iiwa_link_0";
-  collision_object.id = "sensor_mount";
-
-  shape_msgs::SolidPrimitive primitive;
-  primitive.type = primitive.BOX;
-  primitive.dimensions.resize(3);
-  primitive.dimensions[0] = 0.1;
-  primitive.dimensions[1] = 0.1;
-  primitive.dimensions[2] = 0.1;
-
-  geometry_msgs::Pose box_pose;
-  box_pose.position.x = current_pose.pose.position.x;
-  box_pose.position.y = current_pose.pose.position.y;
-  box_pose.position.z = current_pose.pose.position.z;
-  box_pose.orientation.x = current_pose.pose.orientation.x;
-  box_pose.orientation.y = current_pose.pose.orientation.y;
-  box_pose.orientation.z = current_pose.pose.orientation.z + 0.1;
-  box_pose.orientation.w = current_pose.pose.orientation.w;
-
-  collision_object.primitives.push_back(primitive);
-  collision_object.primitive_poses.push_back(box_pose);
-  collision_object.operation = collision_object.ADD;
-
-  std::vector<moveit_msgs::CollisionObject> collision_objects;
-  collision_objects.push_back(collision_object);
-  std::cout << "collision_objects.size(): " << collision_objects.size() << std::endl;
-  std::cout << collision_objects[0] << std::endl;
-  planning_scene_interface.applyCollisionObjects(collision_objects);
-
-  ros::Duration(2.0).sleep();
-
-  std::vector<std::string> obj = planning_scene_interface.getKnownObjectNames();
-  if (obj.size() > 0)
-  {
-    std::cout << std::endl << "-- KNOWN COLLISION OBJECTS --" << std::endl;
-    for (int i = 0; i < obj.size(); ++i)
-      std::cout << obj[i] << std::endl;
-  }
-
-
-  ROS_INFO_NAMED("tutorial", "Add an object into the world");
-  planning_scene_interface.addCollisionObjects(collision_objects);
-
-  ros::Duration(1.0).sleep();
-
-}
 
 
   private:
-    geometry_msgs::PoseStamped current_pose;
+    //geometry_msgs::Pose registered_pose_ = dummy_pose.pose;
+    geometry_msgs::PoseStamped dummy_pose; //pose of the dummy
     geometry_msgs::Pose base_pose_;
+    geometry_msgs::Pose needle_chanal_;
+    geometry_msgs::Pose box_pose;
 
-   
+  public: 
+    // Roboter muss noch entlang des z-Nadelkanal vectors vom einstichpunkt weg bewegt werden
+    geometry_msgs::Pose needle_chanal_pose_; 
+  
   };
 
  } //namespace move_to_target
@@ -150,24 +190,32 @@ public:
   spinner.start();
 
   move_to_target::TargMover registered(&node_handle, "manipulator", "world");
+
+  registered.moveToBasePose(); //go to basePose to switch mode
   
-  ROS_INFO("Please start impedance mode and register your target. Click 'Next' to move to basepose.");
-  
+  ROS_INFO("Please start impedance mode. Move the EEF to the target and register it by pressing 'get' on the SmartPad toolbar. Click 'Next' to move back to the basepose.");
+
   registered.waitForApproval();
 
   registered.moveToBasePose();
 
-  ROS_INFO("Click 'Next' to move to spawn Dummy.");
+  ROS_INFO("Click 'Next' to spawn dummy.");
 
   registered.waitForApproval();
 
-  registered.spawnDummy();
-
- 
+  registered.spawndummy();
 
   ROS_INFO("Click 'Next' to move to the registered pose");
+  
   registered.waitForApproval();
-  registered.moveToRegisteredPose();
+
+  registered.moveToNeedletarget();
+
+  ROS_INFO("Click 'Next' to end");
+
+  registered.waitForApproval();
+
+  // registered.moveToRegisteredPose();
 
   ros::shutdown();
   return 0;
